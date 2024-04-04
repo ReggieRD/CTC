00ebcccbb5a1186ad355321ae70be47bea13e4cb
Dube, RMW, Mr [23684534@sun.ac.za] <23684534@sun.ac.za>
2024-04-04 23:34:33 +0200

    First Commit

---
+++ CTC.java
@@ -0,0 +1,15 @@
+public class CTC {
+	
+	public static int[][] generate_grid (int rowCount){
+		
+	}
+
+	public static int [] extract_row (int rowNr, int[][] grid) {
+
+	}
+
+	public static void main (String [] args) {
+		System.out.println(extract_row(100, generate_grid(105));
+	}
+
+}
